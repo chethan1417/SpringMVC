c4a6b2318cb05caff9ecd127649adf91c8f81499
chethan m <chethan01.xworkz@gmail.com>
2024-12-16 10:14:22 +0530

    Xworkz Admin form

--- xworkz_course_reg/src/main/java/com/xworkz/xworkz_course_reg/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.xworkz_course_reg.repository;
-
-import com.xworkz.xworkz_course_reg.entity.CourseEntity;
-
-public interface CourseRepository {
-    CourseEntity findByEmail(String email);
-
-    boolean save(CourseEntity courseEntity);
-}
