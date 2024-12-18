0629620936df4691f749bbd846d8cb032dab85b9
chethan m <chethan01.xworkz@gmail.com>
2024-12-18 20:52:45 +0530

    forms

---
+++ pro5/src/main/java/com/xworkz/pro5/runner/Runner.java
@@ -0,0 +1,29 @@
+package com.xworkz.pro5.runner;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Runner extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer {
+
+  public Runner()
+  {
+      System.out.println("no arg const of pro5");
+  }
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        String[]  url={"/"};
+        System.out.println("config url for dispatched server");
+        return url;    }
+}
