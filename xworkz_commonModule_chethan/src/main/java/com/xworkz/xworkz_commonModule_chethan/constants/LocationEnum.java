ef87ae6575d0b7034b1a7b598fdc49ad6da099ee
chethan m <chethan01.xworkz@gmail.com>
2025-01-23 22:29:03 +0530

    gym project

--- xworkz_commonModule_chethan/src/main/java/com/xworkz/xworkz_commonModule_chethan/constants/LocationEnum.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.xworkz_commonModule_chethan.constants;
-
-public enum LocationEnum {
-    Bengaluru_Karnataka ,Chitradurga_Karnataka,Mysore_Karnataka,Gadag_Karnataka,Bagalkote_Karnataka;
-}
