241d7c7612d3d92e93cd01bceaca506623de0478
chethan m <chethan01.xworkz@gmail.com>
2025-01-24 00:25:11 +0530

    gym project

--- chethan_gym/src/main/java/com/xworkz/chethan_gym/constants/GymNameEnum.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.chethan_gym.constants;
-
-public enum GymNameEnum {
-
-    Iron_Pulse_Rajajinagar,FlexFit_BTM,Titan_Strength_Indiranagar,Forge_Fitness_HSR_Layout, Peak_Fitness_JP_Nagar;
-}
